da681a49d3744c4f8e13034d28133886ed82a0e2
parkging <parkjun611@gmail.com>
2023-04-07 16:13:22 +0900

    jwt init

--- src/main/java/com/parkging/blog/apiapp/global/config/SecurityConfig.java
+++ src/main/java/com/parkging/blog/apiapp/global/config/SecurityConfig.java
@@ -1,22 +1,21 @@
 package com.parkging.blog.apiapp.global.config;
 
-import com.parkging.blog.apiapp.global.oauth.PrincipalOauth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final PrincipalOauth2UserService principalOauth2UserService;
+    private final CorsFilter corsFilter;
 
     @Bean
     public BCryptPasswordEncoder encodePwd() {
@@ -25,27 +24,18 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        /**
-         * 구글 로그인 완료 후 후처리 =>
-         * 1. 엑세스토큰 + 사용자 프로필정보,
-         * 2. 회원가입 자동 진행
-         */
-        http.authorizeRequests()
+        // 세션 미사용하므로 csrf 비활성화, Stateless 세팅
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(corsFilter)
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeRequests()
                 .antMatchers("/members/**").authenticated()  // 인증 되면 접속
                 .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')") // 인가 되면 접속
                 .anyRequest().permitAll()
-                .and()
-                .formLogin()
-                .usernameParameter("email")
-                .loginPage("/login")
-                .loginProcessingUrl("/login") // /login 주소가 호출이 되면 시큐리티가 대신 낚아채서 로그인 진행
-                .defaultSuccessUrl("/")
-                .and()
-                .oauth2Login()
-                .loginPage("/login")
-                .userInfoEndpoint()
-                .userService(principalOauth2UserService)
-                ;
+        ;
         return http.build();
     }
 
--- src/main/java/com/parkging/blog/apiapp/global/config/WebConfig.java
+++ src/main/java/com/parkging/blog/apiapp/global/config/WebConfig.java
@@ -7,21 +7,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    /**
-     * CORS 전역 설정
-     */
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                // 허용 출처
-                .allowedOrigins("http://localhost:3000")
-                // 허용 HTTP method
-                .allowedMethods("GET", "POST", "PATCH", "DELETE")
-                // 모든 헤더값 참조 허용
-                .exposedHeaders("*")
-                // 쿠키 인증 요청 허용
-                .allowCredentials(true)
-                // pre-flight 리퀘스트 캐싱 시간
-                .maxAge(3000);
-    }
+//    /**
+//     * CORS 전역 설정
+//     */
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                // 허용 출처
+//                .allowedOrigins("http://localhost:3000")
+//                // 허용 HTTP method
+//                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+//                // 모든 헤더값 참조 허용
+//                .exposedHeaders("*")
+//                // 쿠키 인증 요청 허용
+//                .allowCredentials(true)
+//                // pre-flight 리퀘스트 캐싱 시간
+//                .maxAge(3000);
+//    }
 }
