fa6a1df4f7fda475a002cc82548fcbb486611fbe
juny <juny@oscnco.com>
2023-11-16 15:31:34 +0900

    add aws codedeploy files

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+PROJECT_ROOT="/home/ec2-user/blog-backend"
+
+cd $PROJECT_ROOT
+docker-compose up -d
+
+#### jar 배포 주석처리 start ####
+
+#PROJECT_ROOT="/home/ec2-user/prod-blog-backend"
+#JAR_FILE="blog-backend.jar"
+#JAR_FILE_WITH_PATH="$PROJECT_ROOT/build/libs/$JAR_FILE"
+
+#APP_LOG="$PROJECT_ROOT/application.log"
+#ERROR_LOG="$PROJECT_ROOT/error.log"
+#DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+#
+#TIME_NOW=$(date +%c)
+#
+## jar 파일 실행
+#chmod +x $JAR_FILE_WITH_PATH
+#echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+#sudo nohup java -jar $JAR_FILE_WITH_PATH --spring.profiles.active=prod > $APP_LOG 2> $ERROR_LOG &
+#
+#CURRENT_PID=$(pgrep -f $JAR_FILE)
+#echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+
+#### jar 배포 주석처리 end ####
