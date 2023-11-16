1204a890ff17ec651b8cf9d930601e5ccf25a37d
juny <juny@oscnco.com>
2023-11-17 00:43:23 +0900

    edit application_start.sh

--- application_start.sh
+++ application_start.sh
@@ -13,7 +13,7 @@ TIME_NOW=$(date +%c)
 # jar 파일 실행
 chmod +x $JAR_FILE_WITH_PATH
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE_WITH_PATH --spring.profiles.active=prod > $APP_LOG 2> $ERROR_LOG &
+sudo nohup java -jar $JAR_FILE_WITH_PATH --spring.profiles.active=prod > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
