eab9e0f493a328de4ce5bf9964358431e7c91a63
juny <juny@oscnco.com>
2023-11-17 17:27:37 +0900

    edit application_start.sh

--- after_install.sh
+++ after_install.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-PROJECT_ROOT="/home/ec2-user/prod-blog-backend"
-JAR_FILE="blog-backend.jar"
-
-TIME_NOW=$(date +%c)
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-#docker-compose -f $DOCKER_COMPOSE_FILE_WITH_PATH up -d --build &> $DOCKER_COMPOSE_LOG
+docker-compose down >> $DEPLOY_LOG
 
-# 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+#### jar 배포 주석처리 start ####
 
-# 프로세스가 켜져 있으면 종료
-if [ -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
-else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  sudo kill -15 $CURRENT_PID
-fi
+#PROJECT_ROOT="/home/ec2-user/prod-blog-backend"
+#JAR_FILE="blog-backend.jar"
+#
+#TIME_NOW=$(date +%c)
+#DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+#
+##docker-compose -f $DOCKER_COMPOSE_FILE_WITH_PATH up -d --build &> $DOCKER_COMPOSE_LOG
+#
+## 현재 구동 중인 애플리케이션 pid 확인
+#CURRENT_PID=$(pgrep -f $JAR_FILE)
+#
+## 프로세스가 켜져 있으면 종료
+#if [ -z $CURRENT_PID ]; then
+#  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+#else
+#  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+#  sudo kill -15 $CURRENT_PID
+#fi
+
+#### jar 배포 주석처리 end ####
