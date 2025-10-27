c78cfe192c9aa256f7ae7f383e2ef72a32da46a3
parkging <85069674+parkging@users.noreply.github.com>
2025-10-27 16:11:04 +0900

    Dev Merge (#111)
    * add dev settings
    
    * Feature/add prod settings (#107)
    
    * edit githubaction yml
    
    * add master-ect githubaction file
    
    * add dev settings
    
    * [Add] Prod Deploy Settings
    
    ---------
    
    Co-authored-by: juny <juny@oscnco.com>
    
    [Add] Prod Deploy Settings
    
    * [Add] Prod Deploy Settings
    
    * 로컬, 개발, 운영 properties 파일 수정
    
    ---------
    
    Co-authored-by: juny <juny@oscnco.com>

--- src/main/resources/db/migration/V2__create_post_category_table.sql
+++ src/main/resources/db/migration/V2__create_post_category_table.sql
@@ -1,5 +1,3 @@
--- V2__create_post_category_table.sql
--- Create post_category table
 CREATE TABLE post_category (
                                post_category_id BIGINT AUTO_INCREMENT PRIMARY KEY,
                                name VARCHAR(255) NOT NULL,
@@ -9,5 +7,4 @@ CREATE TABLE post_category (
                                FOREIGN KEY (parent_category_id) REFERENCES post_category(post_category_id)
 );
 
--- Index for performance optimization
 CREATE INDEX idx_parent_category_id ON post_category (parent_category_id);
